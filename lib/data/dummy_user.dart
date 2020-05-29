133c385c2a394375101c33f289b99b69e4a71e7a
WeslleyFratini <weeslleey354@gmail.com>
2020-05-29 15:52:58 -0300

    primeiro commit

---
+++ lib/data/dummy_user.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+'1': const User(
+  id:'1',
+  name: 'Maria',
+  email: 'maria@maria.com.br',
+  avatarUrl: 'https://cdn.pixabay.com/photo/2018/04/18/18/56/user-3331256_1280.png'
+),
+'2': const User(
+  id:'2',
+  name: 'Rafael',
+  email: 'rafael@rafael.com.br',
+  avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'
+),
+'3': const User(
+  id:'3',
+  name: 'Weslley',
+  email: 'weslley@weslley.com.br',
+  avatarUrl: 'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651_960_720.png'
+),
+'4': const User(
+  id:'4',
+  name: 'Gabi',
+  email: 'gabi@gabi.com.br',
+  avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/02/17/07/user-307993_960_720.png'
+),
+};
