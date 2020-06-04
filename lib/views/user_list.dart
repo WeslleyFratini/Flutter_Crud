b87d0d42a4b1d1ad5961e5f004a7b3a322588c18
WeslleyFratini <weeslleey354@gmail.com>
2020-06-04 00:13:57 -0300

    mudanças finais

--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/components/user_tile.dart';
 import 'package:flutter_crud/provider/users.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
 import 'package:provider/provider.dart';
 
 class UserList extends StatelessWidget {
@@ -15,8 +16,10 @@ class UserList extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.add),
             onPressed: () {
-              users.remover(users.byIndex(0));              
-            },
+            Navigator.of(context).pushNamed(
+             AppRoutes.USER_FORM
+            );
+          },
           )
         ],
         
